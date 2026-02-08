12049fb7aeda67afc61f2d327e4066495565b6b7
Hunter Poole <punterhoole@gmail.com>
2026-02-07 21:00:11 -0500

    ROUGH prototype complete.
    This prototype is only valuable insofar as to demonstrate a connection
    to Binance. Not likely to use IXWebSockets in the future... should
    probably use Boost.Beast for networking in C++, but I needed a proof of
    concept to be delivered. At this time, I believe we are receiving
    *trades* from binance. Minimally, the JSON they are sending us matches
    that of their "event" response categories in their docs.

--- src/api_demo.cpp
+++ src/api_demo.cpp
@@ -2,18 +2,38 @@
 #include <ixwebsocket/IXNetSystem.h>
 #include <ixwebsocket/IXUserAgent.h>
 #include <ixwebsocket/IXWebSocket.h>
+#include <nlohmann/json.hpp>
 
 int main() {
+
+  nlohmann::json demojson = {
+      {"id", 1},
+      {"method", "SUBSCRIBE"},
+      {"params", {"btcusdt@trade"}},
+  };
+
   ix::WebSocket demoSocket;
 
-  std::string url("wss://stream.binance.com:9443/ws/btcusdt@trade");
+  std::string url("wss://stream.binance.us:9443/ws/btcusdt@trade");
   demoSocket.setUrl(url);
 
-  demoSocket.setOnMessageCallback([](const ix::WebSocketMessagePtr &msg) {
+  demoSocket.setOnMessageCallback([&demoSocket, &demojson](
+                                      const ix::WebSocketMessagePtr &msg) {
     if (msg->type == ix::WebSocketMessageType::Message) {
-      std::cout << "Received: " << msg->str << std::endl;
+      auto data = nlohmann::json::parse(msg->str);
+      if (data.contains("p")) {
+        // auto data = nlohmann::json::parse(msg->str);
+        std::string price = data["p"];
+        std::string quantity = data["q"];
+        std::cout << "Received: " << msg->str << std::endl;
+      } else {
+        std::cout << "Return: " << msg->str << std::endl;
+      }
     } else if (msg->type == ix::WebSocketMessageType::Open) {
-      std::cout << "Successfully connected to Binance" << std::endl;
+      std::cout << "Successfully connected to Binance. Please wait for data to "
+                   "be sent."
+                << std::endl;
+      demoSocket.send(demojson.dump());
     } else if (msg->type == ix::WebSocketMessageType::Error) {
       std::cout << "Error: " << msg->errorInfo.reason << std::endl;
     }
