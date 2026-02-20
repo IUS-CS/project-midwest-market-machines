de6f4b24a07fdd4503cc33741b49c3ecf6ee3180
Duwayne Gray <duwayneceo@gmail.com>
2026-02-19 19:46:30 -0500

    First draft of the Risk layer

---
+++ Risk_Analyzer.cpp
@@ -0,0 +1,10 @@
+// this file contains the implementation of the Risk_Analyzer class, 
+//which is responsible for analyzing the risk of a given market and providing insights to traders.
+#include "Risk_Analyzer.h"
+#include <iostream>
+#include <cmath>
+
+// constructor implementation
+Risk_Analyzer::Risk_Analyzer() {
+    // constructor implementation
+}
