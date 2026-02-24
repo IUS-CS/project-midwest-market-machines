b94f6e2285a42c04d649750e97b7cf7ad8d876a6
Hunter Poole <punterhoole@gmail.com>
2026-02-24 14:23:35 -0500

    Add `#include` statements for `Binance_Websockets.cpp` Also updates `CMakeLists.txt` to output a `compile_commands.json` file into `./build/` to make clang happy when editing `Binance_Websockets.cpp` in nvim. Previously would throw errors that it couldn't find `nhlohmann/json.hpp`

---
+++ src/Binance_Websockets.cpp
@@ -0,0 +1,13 @@
+/* Binance Connection
+ * Backend service for Midwest Market Machines
+ * The Simple Trade App
+ *
+ * Hunter William Poole
+ * 2-24-2026
+ */
+
+#include <iostream>
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXUserAgent.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <nlohmann/json.hpp>
