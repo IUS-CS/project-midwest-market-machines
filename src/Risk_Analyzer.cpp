71787056bcea0f888f0fd3e9a669627adc0617a8
Hunter Poole <punterhoole@gmail.com>
2026-02-25 20:02:16 -0500

    Add `main()` to `Risk_Analyzer.cpp` so make won't fail out on build.

--- src/Risk_Analyzer.cpp
+++ src/Risk_Analyzer.cpp
@@ -1,10 +1,13 @@
-// this file contains the implementation of the Risk_Analyzer class, 
-//which is responsible for analyzing the risk of a given market and providing insights to traders.
+// this file contains the implementation of the Risk_Analyzer class,
+// which is responsible for analyzing the risk of a given market and providing
+// insights to traders.
 #include "Risk_Analyzer.h"
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 // constructor implementation
 Risk_Analyzer::Risk_Analyzer() {
-    // constructor implementation
+  // constructor implementation
 }
+
+int main() { return 0; }
