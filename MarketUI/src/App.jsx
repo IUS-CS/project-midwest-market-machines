4435718a171c0fc07aa69aa40038a3dc0e94e3a6
Tanner Phillips <136934239+tandphill@users.noreply.github.com>
2026-03-10 12:23:28 -0400

    Update App.jsx

--- MarketUI/src/App.jsx
+++ MarketUI/src/App.jsx
@@ -4,33 +4,36 @@ import Navbar from './components/navBar'
 import Watchlist from './components/Watchlist'
 import WebSocket from './WebSocket'
 import ShowPrice from './components/ShowPrice'
-/*
-  Main component, coordinates other files by connecting 
-  1. (currentCoin) state which defaults to BTCUSDT->
-  2. Watchlist component when a user clicks on a coin-> 
-  3. updates (currentCoin) state-> WebSocket function triggers->
-  4. the WebSocket, returning the coin's price->
-  5. passing that price to the ShowPrice component to update the display.
-*/
+import About from "./components/About"
+import Contact from "./components/Contact"
+
 function App() {
-/* (1) Initialization: Sets the starting coin and establishes the socket connection */
   const [currentCoin, setCurrentCoin] = useState('BTCUSDT');
-  /* (4) Data Channel: WebSocket hook listens for changes to currentCoin and returns price */
   const { price } = WebSocket(currentCoin);
+
+  const [page, setPage] = useState("home"); 
 
   return (
     <>
-      <Navbar />
+      <Navbar setPage={setPage} />
 
-      <div className="App_header">
-        <h1>Simple Trade</h1>
-        <p>Your one-stop shop for all things crypto!</p>
-      </div>
-      {/* (2) User Input: Watchlist provides the interface for selecting a new coin */}
-      {/* (3) State Update: onCoin triggers setCurrentCoin, restarting the cycle at step 1 */}
-      <Watchlist onCoin={(c) => setCurrentCoin(c)} currentCoin={currentCoin} />
-      {/* (5) Rendered Output: ShowPrice receives the final price and renders it to the screen */}
-      <ShowPrice price={price} coin={currentCoin} />
+      {page === "home" && (
+        <>
+          <div className="App_header">
+            <h1>Simple Trade</h1>
+            <p>Your one-stop shop for all things crypto!</p>
+          </div>
+
+          {/* (2) User Input: Watchlist provides the interface for selecting a new coin */}
+          {/* (3) State Update: onCoin triggers setCurrentCoin, restarting the cycle at step 1 */}
+          <Watchlist onCoin={(c) => setCurrentCoin(c)} currentCoin={currentCoin} />
+          {/* (5) Rendered Output: ShowPrice receives the final price and renders it to the screen */}
+          <ShowPrice price={price} coin={currentCoin} />
+        </>
+      )}
+
+      {page === "about" && <About />}
+      {page === "contact" && <Contact />}
     </>
   )
 }
