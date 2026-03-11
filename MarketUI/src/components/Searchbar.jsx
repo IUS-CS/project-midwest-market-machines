3df7c997709dd87e7f75478a3a7751fa43822fea
Duwayne Gray <92886171+waynebin@users.noreply.github.com>
2026-03-11 15:37:16 -0400

    Apply suggestion from @hwpoole
    Co-authored-by: Hunter Poole <punterhoole@gmail.com>

--- MarketUI/src/components/Searchbar.jsx
+++ MarketUI/src/components/Searchbar.jsx
@@ -1,37 +1,37 @@
 import { useState } from 'react';
 
 
-// Search component that works with the CandlestickChart component
-// to update the chart as the user searches for different candles.
-// Collects text input and calls onSearch(query) when submitted.
-
-const SearchBar=({onSearch})=>{
-    const [query,setQuery]=useState("");
+const SearchBar= ({onSearch}) => {
+    // Capture the user data.
+    const [query, setQuery] = useState("");
+    
     function submitSearch() {
         const trimmed = query.trim();
         if (!trimmed) return;
+        
         onSearch(trimmed);
         setQuery("");
         }
   
+    // Maybe break this out so it is easier to see.
+    const KeyDownHandler = (e) => {
+        if (e.key === "Enter") {
+            submitSearch();
+        }
+    };
+    
     return(
       <div>
-        <input value={query} 
-        onChange={(e)=> setQuery(e.target.value)}
-        onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                submitSearch();
-              }
-           }}
-        
-        
+        <input
+            value={query} 
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={KeyDownHandler}
         />
         <button onClick={() => {
           submitSearch();
         }}
         >search</button>
       </div>
-
     );
 }
 
