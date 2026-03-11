f158ae06caa76b472f75a217421709375eca0a6d
Hunter Poole <punterhoole@gmail.com>
2026-03-11 00:41:56 -0400

    Increase test robustness. Threads need to sleep to grab a socket, and grab a reply. Test if thread is joinable before joining. Use unsecure "ws://" vs requiring an ssl cert.

--- test/ExchangeClientTest.cpp
+++ test/ExchangeClientTest.cpp
@@ -1,31 +1,32 @@
-/*
+/* ExchangeClientTest
+ * Test file for ExchangeClient.h
+ * The Simple Trade App
  *
+ * ExchangeClientTest.cpp
+ *
+ * This test file tests the functionality of the ExchangeClient class included
+ * in ExchangeClient.cpp. Using the gtest suite, we create a test fixture class,
+ * then use TEST_F(TestFixtureClassName, TestName) {} to specify the tests.
  */
 
 #include "ExchangeClient.h"
-#include "ixwebsocket/IXConnectionState.h"
-#include "ixwebsocket/IXWebSocket.h"
-#include "ixwebsocket/IXWebSocketMessage.h"
-#include "ixwebsocket/IXWebSocketMessageType.h"
 #include "gtest/gtest.h"
-#include <cstddef>
 #include <ixwebsocket/IXWebSocketServer.h>
-#include <thread>
-#include <unistd.h>
 
 using namespace std;
 
 class ExchangeClientTest : public ::testing::Test {
 protected:
+  static atomic<bool> MessageEmpty;
   static unique_ptr<ix::WebSocketServer> Server;
   static thread ServerThread;
-  inline static nlohmann::json BinanceKline = {{"E", "1773198900017"},
+  inline static nlohmann::json BinanceKline = {{"E", 1773198900017},
                                                {"e", "kline"},
                                                {"k",
                                                 {{"B", "0"},
                                                  {"L", "-1"},
                                                  {"Q", "0.00000000"},
-                                                 {"T", "1773198899999"},
+                                                 {"T", 1773198899999},
                                                  {"V", "0.00000000"},
                                                  {"c", "2023.78000000"},
                                                  {"f", "-1"},
@@ -36,7 +37,7 @@ protected:
                                                  {"o", "2023.78000000"},
                                                  {"q", "0.00000000"},
                                                  {"s", "ETHUSDT"},
-                                                 {"t", "1773198840000"},
+                                                 {"t", 1773198840000},
                                                  {"v", "0.00000000"},
                                                  {"x", true}}},
                                                {"s", "ETHUSDT"}};
@@ -50,36 +51,38 @@ protected:
                            ix::WebSocket &webSocket,
                            const ix::WebSocketMessagePtr &msg) -> void {
           if (msg->type == ix::WebSocketMessageType::Open) {
-            webSocket.send(BinanceKline);
+            webSocket.send(BinanceKline.dump(0));
           }
         });
 
     ServerThread = thread([]() { Server->listenAndStart(); });
+    this_thread::sleep_for(chrono::milliseconds(2));
   }
 
   static void TearDownTestSuite() {
     Server->stop();
-    ServerThread.join();
+
+    if (ServerThread.joinable()) {
+      ServerThread.join();
+    }
+
     Server.reset();
   }
 };
 
+atomic<bool> ExchangeClientTest::MessageEmpty{true};
 thread ExchangeClientTest::ServerThread;
 unique_ptr<ix::WebSocketServer> ExchangeClientTest::Server = nullptr;
 
 TEST_F(ExchangeClientTest, SubscribesTo) {
-  bool MessageEmpty = true;
-
   ExchangeClient TestClient;
-  TestClient.SetCallback([&MessageEmpty](const string &msg) -> void {
-    if (msg.empty()) {
-      MessageEmpty = true;
-    } else {
+  TestClient.SetCallback([](const string &msg) -> void {
+    if (!msg.empty()) {
       MessageEmpty = false;
     }
   });
-  TestClient.Connect("wss://127.0.0.1:9999");
+  TestClient.Connect("ws://127.0.0.1:9999");
 
-  // sleep(1);
+  this_thread::sleep_for(chrono::milliseconds(2));
   EXPECT_FALSE(MessageEmpty);
 }
