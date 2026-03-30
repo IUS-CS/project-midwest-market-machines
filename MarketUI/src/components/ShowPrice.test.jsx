5ce8b0bf1895de0728c1350ec4d0b09d69bbfbea
IanOlsen <ijolsen@iu.edu>
2026-03-29 22:24:27 -0400

    Add starter test files within MarketUI for all components but main

---
+++ MarketUI/src/components/ShowPrice.test.jsx
@@ -0,0 +1,7 @@
+import { expect, test, describe, it } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import ShowPrice from './ShowPrice.jsx';
+
+describe('ShowPrice Component', () => {
+  it.todo('should display the correct price and coin name from props');
+});
---
+++ MarketUI/src/components/navBar.test.jsx
@@ -0,0 +1,7 @@
+import { expect, test, describe, it } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import Navbar from './navBar.jsx';
+
+describe('Navbar Component', () => {
+  it.todo('should render the brand logo and navigation links');
+});
