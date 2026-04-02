fb14f6385202c19e311e828a29f36438638410f3
Hunter Poole <punterhoole@gmail.com>
2026-04-01 22:36:14 -0400

    Describe changes in `vite.config.js`

--- MarketUI/vite.config.js
+++ MarketUI/vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
+/* This defineConfig({}) block is used to define two plugins:
+ * 
+ * 1. react.
+ * 2. viteSingleFile.
+ *
+ * React is, well, react.
+ *
+ * viteSingleFile allows us to build the frontend to a single file.
+ * It inlines all assets into one index.html file.
+ * This is required for launching the frontend with WebView.
+ *
+ * base: './' ensures we use *relative paths.*
+ *
+ * https://vite.dev/config/
+ */
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
   base: './',
