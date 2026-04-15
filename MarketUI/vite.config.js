d775e0e78c0df5651f3d2220ac53f2f306d6a148
Hunter Poole <punterhoole@gmail.com>
2026-04-14 20:08:51 -0400

    Prepare UI for switch to shadcn/ui

--- MarketUI/vite.config.js
+++ MarketUI/vite.config.js
@@ -1,17 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+/* vite.config.js
+ *
+ * This file houses the configuration information for Vite.
+ * If any plugins are to be added or removed from the project,
+ * you will be editing this file.
+ */
 
-/* This defineConfig({}) block is used to define two plugins:
+import { defineConfig } from 'vite'                       // Vite build system
+import react from '@vitejs/plugin-react'                  // Vite's React plugin.
+import tailwindcss from '@tailwindcss/vite'               // Tailwindcss required for Shadcn/UI
+import { viteSingleFile } from 'vite-plugin-singlefile'   // Builds the project to a single file for WebView.
+import path from "path"                                   // Required for `path.resolve` in "@" alias.
+import { fileURLToPath } from 'url'                       // Converts a file URL string into an absolute file path.
+
+// Build const __dirname to somewhat match shadcn/ui docs.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/* This defineConfig({...}) block is used to define three plugins:
  * 
  * 1. react.
- * 2. viteSingleFile.
+ * 2. tailwindcss
+ * 3. viteSingleFile.
  *
  * React is, well, react.
+ *
+ * Tailwindcss is required for shadcn/ui. Tailwindcss self-describes
+ * as: "A utility-first CSS framework packed with classes like flex,
+ * pt-4, text-center and rotate-90 that can be composed to build any
+ * design, directly in your markup."
  *
  * viteSingleFile allows us to build the frontend to a single file.
  * It inlines all assets into one index.html file.
  * This is required for launching the frontend with WebView.
+ *
+ * resolve...alias... "@" gives us an absolute path name alias for "@"
  *
  * base: './' ensures we use *relative paths.*
  *
@@ -21,7 +42,16 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
  * https://vite.dev/config/
  */
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: './',
   build: {
     minify: false,
