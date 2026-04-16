b47fa4b88d0d9e646f7ced591e126632b48fa5db
Tanner Phillips <136934239+tandphill@users.noreply.github.com>
2026-04-15 23:52:59 -0400

    Update navBar.jsx
    Adds functionality to navbar.

--- MarketUI/src/components/navBar.jsx
+++ MarketUI/src/components/navBar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
       <div className="navbar-spacer" />
 
         <ul className="navbar-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="#" onClick={() => setPage("home")}>Home</a></li>
+          <li><a href="#" onClick={() => setPage("about")}>About</a></li>
+          <li><a href="#" onClick={() => setPage("contact")}>Contact</a></li>
         </ul>
       </nav>
     </div>
