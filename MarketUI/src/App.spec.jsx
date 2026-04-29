60c04b94a38f79fa828427cbafb853c5ee861b23
IanOlsen <161164499+IanOlsen@users.noreply.github.com>
2026-04-29 17:04:38 -0400

    Update App.spec.jsx to remove deliberate fail behavior

--- MarketUI/src/App.spec.jsx
+++ MarketUI/src/App.spec.jsx
@@ -5,5 +5,5 @@ test.use({ viewport: { width: 500, height: 500 } });
 
 test('should render', async ({ mount }) => {
   const component = await mount(<App />);
-  await expect(component).toContainText('BREAK ON PURPOSE'); 
+  await expect(component).toContainText('Simple Trade'); 
 });
